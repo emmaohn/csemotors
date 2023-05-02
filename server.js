37153033a3cde5780c078f89f4bec4850144394a
emmaohn <joh20123@byui.edu>
2023-05-02 12:52:16 -0600

    CSE motors finished site

--- server.js
+++ server.js
@@ -6,13 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 
 /* ***********************
+ * View Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
  * Routes
  *************************/
+// static route
 app.use(require("./routes/static"))
+
+// index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
